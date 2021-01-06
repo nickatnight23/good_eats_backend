c9d6e2b54cfa160c3b6bd29d3671fb8c14a14e5c
Latricia <tricianickelberry@gmail.com>
2021-01-06 06:55:45 -0800

    location model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+fairfield= Location.create(city:"Fairfield",state:"California")
